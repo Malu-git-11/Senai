5cf5c70b3e93b364a118b35a90b773ef59f6c6b2
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-11-10 11:03:36 -0300

    Projetos Ford<enter>

---
+++ AulaPython/exercicios.py
@@ -0,0 +1,38 @@
+"""
+#Exercício 1
+nome = "Malu"
+
+print(f"Olá {nome}, como vai seu dia?")
+"""
+
+"""
+#Exercício 2
+nome = input("diga seu nome:")
+
+print(f"Olá {nome}, tudo bem?")
+"""
+
+"""
+#Exercício 3
+nome = input("Diga seu nome: ")
+
+print(f"Olá {nome}, você está bem?")
+
+bem = input("sim/não?")
+
+#.lower() transforma todas as letras em minúsculas, 
+#facilitando a comparação independentemente de como o usuário digite a resposta.
+
+if bem.lower() == "sim":
+    print("Que bom, tenha um bom dia!")
+else:
+    print("Sinto muito, espero que você melhore.")
+"""
+
+#Exercício 4 - Comentário da linguagem Python
+
+"""
+Achei a linguagem Python até então bem interessante e fácil de fazer.
+A história da linguagem também é legal,
+principalmente o fato de ter sido criada por alguém que trabalhava com inteligência artificial.
+"""
--- AulaPython/helloWorld.py
+++ AulaPython/helloWorld.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+"""
+nome = input("Diga seu nome: ")
+idade = input("Diga sua idade:")
+anoAtual = 2025
+anoNasc = (anoAtual - int(idade))
+
+#Utilizando a vírgula para separar os valores na função print
+print("Olá ",nome,", você tem ",idade," anos e nasceu em ",anoNasc,".")
+
+#Utilizando a função f-string (formatação de strings)
+print(f"Olá {nome}, você tem {idade} anos e nasceu em {anoNasc}.")
+"""
+
+#Teste de erro com condicional
+if True:
+    print("Isso vai dar errado!")
+    
