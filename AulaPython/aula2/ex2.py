71ed3ba9fc81f3767ac331ed38dfabacd0bb83d9
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-11-11 11:27:39 -0300

    Segunda aula de Python, operadores lógicos.

---
+++ AulaPython/aula2/ex2.py
@@ -0,0 +1,65 @@
+"""
+#Exercício 1
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+print("Os resultados são: ")
+print("Soma: ", num1 + num2)
+print("Subtração: ", num1 - num2)
+print("Multiplicação: ", num1 * num2)
+print("Divisão: ", num1 / num2)
+print("Exponenciação: ", num1 ** num2)
+print("Divisão Inteira: ", num1 // num2)
+print("Módulo: ", num1 % num2)
+"""
+
+"""
+#Exercício 2
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+media = (num1 + num2) / 2
+
+print("A média é: ", media)
+"""
+
+"""
+#Exercício 3
+
+anoNasc = input("Diga seu ano de nascimento:")
+anoAtual = 2025
+idade = (anoAtual - int(anoNasc))
+
+print("Você tem ",idade," anos.")
+"""
+
+"""
+#Exercício 4
+
+tempCelsius  = float(input("Digite a temperatura em Celsius: "))
+tempKelvin = tempCelsius + 273.15
+
+print("A temperatura em Kelvin é: ", tempKelvin)
+"""
+
+"""
+#Exercício 5
+
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+area = base * altura
+
+print("")
+"""
+
+"""
+#Exercício 6
+lado = int(input("Digite o valor do lado do quadrado: "))
+
+perimetro = lado * 4
+
+print("O perímetro do quadrado é: ", perimetro)
+"""
---
+++ AulaPython/aula2/exemplo.py
@@ -0,0 +1,12 @@
+texto = "2"
+texto1 = str("Malu")
+num = 3
+num1 = int(4)
+num2 = float(5.5)
+num3 = 3.0
+print(texto)
+print(texto1)
+print(num)
+print(num1)
+print(num2)
+print(num3)
