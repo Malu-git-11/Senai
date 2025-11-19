498c317d9331dbca06cd771b4dd036cd66b0cdb2
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-11-19 09:21:16 -0300

    Aula 5 e 6 adicionada

---
+++ AulaPython/aula5/for.py
@@ -0,0 +1,17 @@
+""" Exemplo 1
+for n1 in [1,2, 3, 8]:
+    print(n1)
+    
+seq = ['a', 'b', 'c', 'd']
+for n2 in seq:
+    print(n2)
+"""
+""" Exemplo 2
+numero = int(input("Digite um número:"))
+total = int(input("Digite um número total: "))
+passagem = int(input("Digite uma progressão aritmética: "))
+
+for i in range(numero, total, passagem):
+    print(i)
+"""
+
---
+++ AulaPython/aula6/lista.py
@@ -0,0 +1,8 @@
+"""
+bancos = ["Banco di Brasil", "Caixa", "Santander"]
+print(bancos)
+bancos = bancos + ["Bradesco", "Itaú"]
+print(bancos)
+bancos += [input("Digite um banco: ")]
+print(bancos)
+"""
